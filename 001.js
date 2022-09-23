5f3c386020e922109a0b5b4959e1ebff4c22e82c
Kateryna Ostrynska <kateriinag@gmail.com>
2022-09-23 13:30:58 +0300

    update 001

--- 001.js
+++ 001.js
@@ -1,9 +1,35 @@
 const mango = {
-  username: "Mango",
+  username: 'Mango',
   showUsername() {
     console.log(this);
     console.log(`My username is: ${this.username}`);
   },
 };
 
-const btn = document.querySelector(".js-btn");
+
+
+HTML CSS JSResult Skip Results Iframe
+EDIT ON
+const addListenerBtn = document.querySelector('[data-action="add"]');
+const removeListenerBtn = document.querySelector('[data-action="remove"]');
+const btn = document.querySelector("#btn");
+
+const handleClick = () => {
+  console.log("click event listener callback");
+};
+
+addListenerBtn.addEventListener("click", () => {
+  btn.addEventListener("click", handleClick);
+  console.log("click event listener was added to btn");
+});
+
+removeListenerBtn.addEventListener("click", () => {
+  btn.removeEventListener("click", handleClick);
+  console.log("click event listener was removed from btn");
+});
+
+const btn = document.querySelector('.js-btn');
+
+mango.showUsername();
+
+btn.addEventListener('click', mango.showUsername.bind(mango));
