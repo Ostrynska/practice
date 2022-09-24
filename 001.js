ee59461ac2be67c1a5406e09ddd226a507e37ce6
Kateryna Ostrynska <kateriinag@gmail.com>
2022-09-24 10:54:19 +0300

    Update 001.js

--- 001.js
+++ 001.js
@@ -90,3 +90,29 @@ function removeChar(str) {
 }
 
 // console.log(removeChar('eloquent'));
+
+function alphabetPosition(text) {
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  return text.toLowerCase().split('')
+  .filter( letter => {
+    let index = alphabet.indexOf(letter);
+    return index > -1;
+  }  )
+  .map( letter => alphabet.indexOf(letter) + 1 )
+  .join(' ')
+}
+
+/ const makeChangeColor = function(){
+//   const changeColor = function(color){
+//     this.color = color;
+//     console.log('changeColor -> this', this)
+//   }
+//    const sweater = {
+//     color:'teal',
+//    }
+//    sweater.updateColor = changeColor;
+
+//    return sweater.updateColor.bind(sweater);
+// }
+// const sweatColor = makeChangeColor();
+// sweatColor('blue');
